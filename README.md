4fc756296eae0740343ee19d69edbc34b748b08d
Pradeep <webreinvent@gmail.com>
2019-12-01 00:52:13 +0530

    package development is completed

--- README.md
+++ README.md
@@ -1,2 +1,145 @@
-# laravel-cpanel
-Laravel CPanel API 2.0
+# Laravel CPanel
+
+> Laravel Package for CPanel UAPI  
+  
+Please consider starring the project to show your :heart: and support.  
+
+> This laravel package allows you to manage you CPanel based hosting using CPanel UAPI. 
+
+Some practical usages are:
+- Programmatically create database, sub domains, emails or accounts etc
+- Programmatically create database users
+- Programmatically set privileges on database of any user
+
+Learn more about at [CPanel UAPI](https://documentation.cpanel.net/display/DD/Guide+to+UAPI)
+
+## Installation 
+
+#### Step 1) Install the Package
+Use following composer command to install the package
+```bash  
+composer require webreinvent/laravel-cpanel  
+```
+or
+Add `webreinvent/laravel-cpanel` as a requirement to `composer.json`:
+
+```
+{
+    ...
+    "require": {
+        ...
+        "webreinvent/laravel-cpanel": "dev-master"
+    },
+}
+```
+
+Update composer:
+
+```
+$ composer update
+```
+#### Step 2) Register the ServiceProvider
+Add following service provider in `config/app.php`  
+```php  
+/*  
+ * Package Service Providers...  
+ */ 
+ 'providers' => [  
+        //...  
+        WebReinvent\CPanel\CPanelServiceProvider::class,   
+        //...  
+    ],
+```
+
+#### Step 3) Publish Configurations
+Run following command:
+```
+php artisan vendor:publish --provider="WebReinvent\CPanel\CPanelServiceProvider" --tag=config
+```
+#### Step 4) Set CPanel details in `.env`
+```
+CPANEL_DOMAIN= 
+CPANEL_PORT=
+CPANEL_API_TOKEN=
+CPANEL_USERNAME=
+```
+To generate `CPANEL_API_TOKEN`, login to the `CPanel >> SECURITY >> Manage API Tokens >> Create`.
+
+## Usages & available methods 
+Make sure you import:
+```php
+use WebReinvent\CPanel\CPanel;
+```
+
+#### To Create Database
+Database name should be prefixed with cpanel username `cpanelusername_databasename`
+
+If your CPanel username is `foo` then your database name 
+| should be `foo_website`.
+
+```php
+$cpanel = new CPanel();  
+$response = $cpanel->createDatabase('cpanelusername_databasename');
+```
+Find More Details at [CPanel UAPI - Mysql::create_database](https://documentation.cpanel.net/display/DD/UAPI+Functions+-+Mysql::create_database)
+
+#### To Delete Database
+
+```php
+$cpanel = new CPanel();  
+$response = $cpanel->deleteDatabase('cpanelusername_databasename');
+```
+
+[CPanel UAPI - Mysql::delete_database](https://documentation.cpanel.net/display/DD/UAPI+Functions+-+Mysql%3A%3Adelete_database)
+
+#### To Get List of All Databases in the CPanel
+
+```php
+$cpanel = new CPanel();  
+$response = $cpanel->listDatabases();
+```
+#### To Create Database User
+
+```php
+$cpanel = new CPanel();  
+$response = $cpanel->createDatabaseUser($username, $password);
+```
+#### To Delete Database User
+
+```php
+$cpanel = new CPanel();  
+$response = $cpanel->deleteDatabaseUser($username);
+```
+
+#### To Give All Privileges to a Database User On a Database
+
+```php
+$cpanel = new CPanel();  
+$response = $cpanel->setAllPrivilegesOnDatabase($database_user, $database_name);
+```
+
+Those where the available method but you can call all the method available at  [CPanel UAPI](https://documentation.cpanel.net/display/DD/Guide+to+UAPI) using following method:
+```php
+$cpanel = new CPanel();  
+$response = $cpanel->callUAPI($Module, $function, $parameters_array);
+```
+Example if you want to add new `ftp` account, documetation is available at [CPanel UAPI - Ftp::add_ftp](https://documentation.cpanel.net/display/DD/UAPI+Functions+-+Ftp%3A%3Aadd_ftp) then use the method as represented below:
+```php
+$cpanel = new CPanel();  
+$Method = 'Ftp';
+$function = 'add_ftp';
+$parameters_array = [
+'user'=>'ftp_username',
+'pass'=>'ftp_password', //make sure you use strong password
+'quota'=>'42',
+];
+$response = $cpanel->callUAPI($Module, $function, $parameters_array);
+```
+## Support us  
+  
+[WebReinvent](https://www.webreinvent.com) is a web agency based in Delhi, India. You'll find an overview of all our open source projects [on github](https://github.com/webreinvent).  
+  
+## License  
+  
+The MIT License (MIT). Please see [License File](LICENSE) for more information.  
+ 
---
+++ src/CPanel.php
@@ -0,0 +1,197 @@
+<?php
+/**
+ * CPanel
+ *
+ * @author: Pradeep Kumar
+ * This package uses cPanel API 2.0
+ */
+
+namespace WebReinvent\CPanel;
+
+use Config;
+
+class CPanel {
+
+    protected $config;
+    protected $protocol;
+    protected $domain;
+    protected $port;
+    protected $user;
+    protected $token;
+
+
+    //-----------------------------------------------------
+
+    public function __construct()
+    {
+        $this->config = Config::get('cpanel');
+
+        $this->protocol = $this->config['protocol'];
+        $this->domain = $this->config['domain'];
+        $this->port = $this->config['port'];
+        $this->username = $this->config['username'];
+        $this->token = $this->config['api_token'];
+
+    }
+
+    //-----------------------------------------------------
+
+    public function createSubDomain($subdomain, $rootdomain, $dir)
+    {
+        $module = "SubDomain";
+        $function = "addsubdomain";
+        $parameters = array(
+            'domain'        => $subdomain,
+            'rootdomain'    => $rootdomain,
+            'canoff'        => 0,
+            'dir'           => $dir,
+            'disallowdot'   => 0
+        );
+        return $this->call($module, $function, $parameters);
+    }
+    //-----------------------------------------------------
+
+    //-----------------------------------------------------
+
+    public function createDatabase($database_name)
+    {
+        $module = "Mysql";
+        $function = "create_database";
+        $parameters = array(
+            'name'    => $database_name
+        );
+        return $this->call($module, $function, $parameters);
+    }
+
+    //-----------------------------------------------------
+    public function deleteDatabase($database_name)
+    {
+        $module = "Mysql";
+        $function = "delete_database";
+        $parameters = array(
+            'name'    => $database_name
+        );
+        return $this->call($module, $function, $parameters);
+    }
+    //-----------------------------------------------------
+    public function listDatabases()
+    {
+        $module = "Mysql";
+        $function = "list_databases";
+        $parameters = array(
+        );
+        return $this->call($module, $function, $parameters);
+    }
+    //-----------------------------------------------------
+    public function createDatabaseUser($username, $password)
+    {
+        $module = "Mysql";
+        $function = "create_user";
+        $parameters = array(
+            'name'    => $username,
+            'password'    => $password,
+        );
+        return $this->call($module, $function, $parameters);
+    }
+
+    //-----------------------------------------------------
+    public function deleteDatabaseUser($username)
+    {
+        $module = "Mysql";
+        $function = "delete_database";
+        $parameters = array(
+            'name'    => $username
+        );
+        return $this->call($module, $function, $parameters);
+    }
+    //-----------------------------------------------------
+    public function setAllPrivilegesOnDatabase($database_user, $database_name)
+    {
+        $module = "Mysql";
+        $function = "set_privileges_on_database";
+        $parameters = array(
+            'user'    => $database_user,
+            'database'    => $database_name,
+            'privileges'    => 'ALL PRIVILEGES',
+        );
+        return $this->call($module, $function, $parameters);
+    }
+    //-----------------------------------------------------
+
+
+    //-----------------------------------------------------
+    public function callUAPI($Module, $function, $parameters_array = array())
+    {
+        $this->call($Module, $function, $parameters_array);
+    }
+    //-----------------------------------------------------
+
+    public function call($module, $function, $args = array())
+    {
+        $parameters = '';
+        if ( count($args) > 0 ) {
+            foreach( $args as $key => $value ) {
+                $parameters .= '&' . $key . '=' . $value;
+            }
+        }
+
+        $url = $this->protocol.'://'.$this->domain . ':' . $this->port . '/execute/' . $module;
+        $url .= "/".$function;
+        $url .= '?'. $parameters;
+
+        $headers = array(
+            "Authorization: cpanel " . $this->username . ':' . $this->token,
+            "cache-control: no-cache"
+        );
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_PORT => $this->port,
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_POSTFIELDS => "",
+            CURLOPT_HTTPHEADER => $headers,
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+
+        if ($err) {
+
+            $response['status'] = 'failed';
+            $response['errors'] = $err;
+            $response['inputs']['url'] = $url;
+
+        } else {
+
+            $res = json_decode($response);
+
+            $response = [];
+            if($res->status == 0)
+            {
+                $response['status'] = 'failed';
+                $response['errors'][] = $res->errors;
+                $response['inputs']['url'] = $url;
+            } else
+            {
+                $response['status'] = 'success';
+                $response['data'] = $res;
+                $response['inputs']['url'] = $url;
+            }
+        }
+
+        return $response;
+    }
+
+    //-----------------------------------------------------
+
+}
